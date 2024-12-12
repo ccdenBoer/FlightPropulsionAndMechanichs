ef66b35269824ff203c109354d760c124b406fe3
Tom <454147@student.fontys.nl>
2024-12-12 10:23:42 +0100

    Added 3 different altitudes
    Altitudes ranges are
    0 feet (at sea level)
    10.000 feet
    30.000 feet

--- week3.py
+++ week3.py
@@ -2,14 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def powerRequiredCurve(x):
+a = 0.0494
+rho = 1.225
+S = 361.6
+W = 217000
+b = 0.03164114177
 
-    a = 0.0494
+def powerRequiredCurveAtSeaLevel(x):
+
     rho = 1.225
-    S = 361.6
-    W = 217000
-    b = 0.03164114177
+    return (a * rho * S)/(2) * x**3 + (2 * b * W**2)/(rho * x * S)
 
+def powerRequiredCurveAt10KFeet(x):
+
+    rho = 1.1116 
+    return (a * rho * S)/(2) * x**3 + (2 * b * W**2)/(rho * x * S)
+
+def powerRequiredCurveAt30KFeet(x):
+
+    rho = 0.459
     return (a * rho * S)/(2) * x**3 + (2 * b * W**2)/(rho * x * S)
 
 def powerAvailableCurve(x):
@@ -18,20 +29,32 @@ def powerAvailableCurve(x):
 
 def graph():
     # Create a range of x values
-    HGx = np.linspace(0, 200, 1000)  # 1000 points between 0 and 200
+    HGx = np.linspace(0, 300, 1000)  # 1000 points between 0 and 400
     # Compute the corresponding y values
-    HGy = powerRequiredCurve(HGx)
+    HGSeaLevel = powerRequiredCurveAtSeaLevel(HGx)
+    HG10KFeet = powerRequiredCurveAt10KFeet(HGx)
+    HG30KFeet = powerRequiredCurveAt30KFeet(HGx)
+
     HGa = powerAvailableCurve(HGx)
 
 
     # Plot the function
     plt.figure(figsize=(10, 10))
-    plt.plot(HGx, HGy, label=r"Power Required Curve", color='blue')
-    plt.plot(HGx, HGa, label=r"Power Available", color="red")
+    plt.plot(HGx, HGSeaLevel, label=r"Sea level", color='lightblue')
+    plt.plot(HGx, HG10KFeet, label=r"10.000 Feet", color='blue')
+    plt.plot(HGx, HG30KFeet, label=r"30.000 Feet", color='darkblue')
+
+    plt.plot(HGx, HGa, label=r"Thrust Available", color="red")
 
     # Highlight intersection
-    idx = np.argwhere(np.diff(np.sign(HGy - HGa))).flatten()
-    plt.plot(HGx[idx], HGa[idx], 'ro')
+    intersectionsSeaLevel = np.argwhere(np.diff(np.sign(HGSeaLevel - HGa))).flatten()
+    intersections10KFeet = np.argwhere(np.diff(np.sign(HG10KFeet - HGa))).flatten()
+    intersections30KFeet = np.argwhere(np.diff(np.sign(HG30KFeet - HGa))).flatten()
+    
+    plt.plot(HGx[intersectionsSeaLevel], HGa[intersectionsSeaLevel], 'ro')
+    plt.plot(HGx[intersections10KFeet], HGa[intersections10KFeet], 'ro')
+    plt.plot(HGx[intersections30KFeet], HGa[intersections30KFeet], 'ro')
+
 
     plt.axhline(0, color='black', linewidth=0.7, linestyle='--')  # x-axis
     plt.axvline(0, color='black', linewidth=0.7, linestyle='--')  # y-axis
